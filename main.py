131f84c5efff12e21ea0df2f5fe2b16439416a96
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2025-06-16 23:03:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,51 +11,86 @@ This is useful for musicians to transpose easier to other keys.
 
 """
 
-# Creates a list of all the Chords in the Key of G
-G_chords = []
-
-chordsFile = open("keyofG.txt", "r")
-
-for chord in chordsFile:
-    G_chords.append(chord.strip())
-
-# Reads the Song Input File
-songFile = open("songInput.txt", "r")
-
-songFileString = songFile.read()
-
-# Checks if the Song File is Empty
-if len(str(songFileString)) == 0:
-    print("Please update song.txt with the chords of your song in the key of G")
+def chordAnalysis():
+    global songFileString,chordNumbers,G_chords,keyChords,keys
     
-else:
-    print("__________ChordMD_________"+ "\n")
+    # Lists
+    G_chords = []
+    chordNumbers  = []
+    keyChords = []
+    keys = []
+    
+    # Reads the Song Input File
+    songFile = open("songInput.txt", "r")
+    songFileString = songFile.read()
+    
+    # Creates a list of all the Chords in the Key of G
+    chordsFile = open("keyofG.txt", "r")
 
-numberSystemFile = open("numberSystem.txt", "r")
+    for chord in chordsFile:
+        G_chords.append(chord.strip())
 
-# Creates a list for the Nashville Number System
-chordNumbers  = []
+    # Creates a list for the Nashville Number System
+    numberSystemFile = open("numberSystem.txt", "r")
+    for chord in numberSystemFile:
+        chordNumbers.append(chord.strip())
+        
+    
+    # Create a Dictionary for the Chords of Each Key (Key:List of Chords)
+    chordDatabase = open("chordsDatabase.csv", "r")
+    
+    # Reading the Chords Database and Creating a List for Each Key and List of all Keys
+    for chordGroup in chordDatabase:
+        keyChords.append(chordGroup.split(","))
+        
+    for key in keyChords:
+        keys.append(key[0])
+    
+    print()
+        
+  
+    
+def replaceChords():
+    global songFileUpdated
+    
+    songFileUpdated = songFileString.replace(keyChords[keyIndex][13], str(chordNumbers[13]))\
+        .replace(keyChords[keyIndex][15],str(chordNumbers[15]))\
+            .replace(keyChords[keyIndex][1],str(chordNumbers[1]))\
+                .replace(keyChords[keyIndex][2],str(chordNumbers[2]))\
+                    .replace(keyChords[keyIndex][3],str(chordNumbers[3]))\
+                        .replace(keyChords[keyIndex][4],str(chordNumbers[4]))\
+                            .replace(keyChords[keyIndex][5],str(chordNumbers[5]))\
+                                .replace(keyChords[keyIndex][6],str(chordNumbers[6]))\
+                                    .replace(keyChords[keyIndex][7],str(chordNumbers[7]))\
+                                        .replace(keyChords[keyIndex][8],str(chordNumbers[8]))\
+                                            .replace(keyChords[keyIndex][9],str(chordNumbers[9]))\
+                                                .replace(keyChords[keyIndex][10],str(chordNumbers[10]))\
+                                                    .replace(keyChords[keyIndex][11],str(chordNumbers[11]))\
+                                                        .replace(keyChords[keyIndex][12],str(chordNumbers[12]))\
+                                                                .replace(keyChords[keyIndex][14],str(chordNumbers[14]))\
+                                                                        .replace(keyChords[keyIndex][16],str(chordNumbers[16]))\
+                                                                            .replace(keyChords[keyIndex][0],str(chordNumbers[0]))
 
-for chord in numberSystemFile:
-    chordNumbers.append(chord.strip())
-
-songFileUpdated = songFileString.replace(G_chords[13], str(chordNumbers[13]))\
-.replace(G_chords[15],str(chordNumbers[15]))\
-    .replace(G_chords[1],str(chordNumbers[1]))\
-        .replace(G_chords[2],str(chordNumbers[2]))\
-            .replace(G_chords[3],str(chordNumbers[3]))\
-                .replace(G_chords[4],str(chordNumbers[4]))\
-                    .replace(G_chords[5],str(chordNumbers[5]))\
-                        .replace(G_chords[6],str(chordNumbers[6]))\
-                            .replace(G_chords[7],str(chordNumbers[7]))\
-                                .replace(G_chords[8],str(chordNumbers[8]))\
-                                    .replace(G_chords[9],str(chordNumbers[9]))\
-                                        .replace(G_chords[10],str(chordNumbers[10]))\
-                                            .replace(G_chords[11],str(chordNumbers[11]))\
-                                                .replace(G_chords[12],str(chordNumbers[12]))\
-                                                        .replace(G_chords[14],str(chordNumbers[14]))\
-                                                                .replace(G_chords[16],str(chordNumbers[16]))\
-                                                                    .replace(G_chords[0],str(chordNumbers[0]))
 
 
-print(songFileUpdated)
+def main():
+    chordAnalysis()
+
+    # Checks if the Song File is Empty
+    if len(str(songFileString)) == 0:
+        print("Please update song.txt with the chords of your song!")
+    
+    else:
+        print("__________ChordMD_________"+ "\n")
+    
+    global keyIndex
+    keyStr = input("Please enter the key of your song: ")
+    
+    keyIndex = keys.index(keyStr)
+    
+    replaceChords()
+
+    print(songFileUpdated)
+    
+if __name__ == "__main__":
+    main()
