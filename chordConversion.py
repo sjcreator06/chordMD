760df91e58be56133f7784eb3437e5c9e68ae3b3
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2025-06-17 00:10:31 -0700

    Update chordConversion.py

--- chordConversion.py
+++ chordConversion.py
@@ -3,90 +3,89 @@ __________________________________ChordMD__________________________________
 
 This program asks the user to input the chords in any key. 
 
-The script replaces the chords with the Nashville Number System chords. 
+The script replaces chords with the Nashville Number System chords. 
 
 This is useful for musicians to transpose easier to other keys.
 
                                ©sjcreator06
-
 """
 
+from fpdf import FPDF
+
 def chordAnalysis():
-    global songFileString,chordNumbers,G_chords,keyChords,keys
-    
-    # Lists
-    chordNumbers  = []
+    global songFileString, chordNumbers, G_chords, keyChords, keys
+
+    chordNumbers = []
     keyChords = []
     keys = []
-    
-    # Reads the Song Input File
-    songFile = open("Data/songInput.txt", "r")
-    songFileString = songFile.read()
 
-    # Creates a list for the Nashville Number System
-    numberSystemFile = open("Data/numberSystem.txt", "r")
-    for chord in numberSystemFile:
-        chordNumbers.append(chord.strip())
-        
-    
-    # Create a Dictionary for the Chords of Each Key (Key:List of Chords)
-    chordDatabase = open("Data/chordsDatabase.csv", "r")
-    
-    # Reading the Chords Database and Creating a List for Each Key and List of all Keys
-    for chordGroup in chordDatabase:
-        keyChords.append(chordGroup.split(","))
-        
+    with open("Data/songInput.txt", "r") as songFile:
+        songFileString = songFile.read()
+
+    with open("Data/numberSystem.txt", "r") as numberSystemFile:
+        for chord in numberSystemFile:
+            chordNumbers.append(chord.strip())
+
+    with open("Data/chordsDatabase.csv", "r") as chordDatabase:
+        for chordGroup in chordDatabase:
+            keyChords.append(chordGroup.strip().split(","))
+
     for key in keyChords:
         keys.append(key[0])
-    
-    print()
-        
-  
-    
+
 def replaceChords():
     global songFileUpdated
-    
-    songFileUpdated = songFileString.replace(keyChords[keyIndex][13], str(chordNumbers[13]))\
-        .replace(keyChords[keyIndex][15],str(chordNumbers[15]))\
-            .replace(keyChords[keyIndex][1],str(chordNumbers[1]))\
-                .replace(keyChords[keyIndex][2],str(chordNumbers[2]))\
-                    .replace(keyChords[keyIndex][3],str(chordNumbers[3]))\
-                        .replace(keyChords[keyIndex][4],str(chordNumbers[4]))\
-                            .replace(keyChords[keyIndex][5],str(chordNumbers[5]))\
-                                .replace(keyChords[keyIndex][6],str(chordNumbers[6]))\
-                                    .replace(keyChords[keyIndex][7],str(chordNumbers[7]))\
-                                        .replace(keyChords[keyIndex][8],str(chordNumbers[8]))\
-                                            .replace(keyChords[keyIndex][9],str(chordNumbers[9]))\
-                                                .replace(keyChords[keyIndex][10],str(chordNumbers[10]))\
-                                                    .replace(keyChords[keyIndex][11],str(chordNumbers[11]))\
-                                                        .replace(keyChords[keyIndex][12],str(chordNumbers[12]))\
-                                                                .replace(keyChords[keyIndex][14],str(chordNumbers[14]))\
-                                                                        .replace(keyChords[keyIndex][16],str(chordNumbers[16]))\
-                                                                            .replace(keyChords[keyIndex][0],str(chordNumbers[0]))
 
+    songFileUpdated = songFileString
+    for i in range(len(chordNumbers)):
+        songFileUpdated = songFileUpdated.replace(keyChords[keyIndex][i], str(chordNumbers[i]))
+
+def createFormattedPDF(songName, artistName, content):
+    pdf = FPDF(format='letter')
+    pdf.set_auto_page_break(auto=True, margin=15)
+    pdf.add_page()
+
+    # Title
+    pdf.set_font("Arial", 'B', 16)
+    pdf.cell(0, 10, f"{songName}", ln=True, align='C')
+
+    # Artist
+    pdf.set_font("Arial", 'I', 12)
+    pdf.cell(0, 10, f"by {artistName}", ln=True, align='C')
+
+    pdf.ln(10)
+
+    # Chord content
+    pdf.set_font("Courier", size=11)
+    for line in content.splitlines():
+        pdf.multi_cell(0, 8, line)
+
+    pdf.output(artistName + "_" + songName + "_" +  ".pdf")
 
 
 def main():
     chordAnalysis()
 
-    # Checks if the Song File is Empty
-    if len(str(songFileString)) == 0:
-        print("Please update song.txt with the chords of your song!")
-    
-    else:
-        print("__________ChordMD_________"+ "\n")
-    
+    if len(songFileString.strip()) == 0:
+        print("Please update songInput.txt with the chords of your song!")
+        return
+
+    print("__________ChordMD_________\n")
+
     songName = input("Please enter the name of your song: ")
-    
-    global keyIndex
+    artistName = input("Please enter the name of the artist: ")
     keyStr = input("Please enter the key of your song: ")
-    
-    
-    keyIndex = keys.index(keyStr)
-    
+
+    global keyIndex
+    keyIndex = keys.index(keyStr.upper())
+
     replaceChords()
 
+    createFormattedPDF(songName, artistName, songFileUpdated)
+
+    print("\nFormatted song PDF saved as 'ChordMD_Output.pdf'.")
+    print("\n--- Converted Song ---\n")
     print(songFileUpdated)
-    
+
 if __name__ == "__main__":
     main()
